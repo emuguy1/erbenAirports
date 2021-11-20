317074551691d39d5397b83d9f881e621d2b6103
emuguy1 <emanuel.erben@gmail.com>
2021-11-20 13:58:15 +0100

    spring initializer

---
+++ src/main/java/de/othr/eerben/erbenairports/ErbenairportsApplication.java
@@ -0,0 +1,13 @@
+package de.othr.eerben.erbenairports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErbenairportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErbenairportsApplication.class, args);
+	}
+
+}
