fe9f570a19e50995118527235eb948313dab49e5
emuguy1 <emanuel.erben@gmail.com>
2021-11-29 21:28:05 +0100

    fixed CustomerRepository

--- src/main/java/de/othr/eerben/erbenairports/backend/data/repositorys/CustomerRepository.java
+++ src/main/java/de/othr/eerben/erbenairports/backend/data/repositorys/CustomerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface CustomerRepository extends CrudRepository<Customer,Long> {
-    Optional<Customer> findByUserName(String username);
+    Optional<Customer> findBySurname(String surname);
 }
