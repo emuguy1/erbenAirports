9f33f6058a54a877949d553c0be89fbd3f5e59fa
emuguy1 <emanuel.erben@gmail.com>
2021-12-01 23:43:56 +0100

    Added Flightdetails attributes with numbers to relations. Also had to change database config - again . dont know why

--- src/main/java/de/othr/eerben/erbenairports/backend/data/entities/Flightdetails.java
+++ src/main/java/de/othr/eerben/erbenairports/backend/data/entities/Flightdetails.java
@@ -1,4 +1,51 @@
 package de.othr.eerben.erbenairports.backend.data.entities;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Flightdetails {
+
+    @Id
+    @NotNull
+    private String flightnumber;
+
+    @NotNull
+    private double flightTimeHours;
+
+    @NotNull
+    private double maxCargo;
+
+    @NotNull
+    private int passangerCount;
+
+    @NotNull
+    @ManyToOne
+    private Airport departure;
+
+    @NotNull
+    @ManyToOne
+    private Airport origin;
+
+    @NotNull
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private Employee createdBy;
+
+    @OneToOne
+    private Calendarslot departureTime;
+
+    @OneToOne
+    private Calendarslot arrivalTime;
+
+    public Flightdetails(){}
+
+
+
 }
