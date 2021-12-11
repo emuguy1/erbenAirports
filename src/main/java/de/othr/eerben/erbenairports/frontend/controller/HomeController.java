f4472a8f7fb4640e6939778e02d90544e49a44b6
emuguy1 <emanuel.erben@gmail.com>
2021-12-11 16:37:46 +0100

    Added icons pictures and home configs

--- src/main/java/de/othr/eerben/erbenairports/frontend/controller/HomeController.java
+++ src/main/java/de/othr/eerben/erbenairports/frontend/controller/HomeController.java
@@ -1,4 +1,26 @@
 package de.othr.eerben.erbenairports.frontend.controller;
 
+import de.othr.eerben.erbenairports.backend.data.entities.Customer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String start(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET) // /login
+    public String login(Model model) {
+        model.addAttribute("user", new Customer());
+        return "authentication/login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST) // th:action="@{/login}"
+    public String doLogin() {
+        return "index";
+    }
 }
